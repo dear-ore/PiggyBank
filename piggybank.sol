0ac05b99b07afeba446e23eb9ecfa1cf52f3f67c
dear-ore <adorable2209@gmail.com>
2022-06-05 16:01:25 +0100

    used SafeMath to check overflows and underflows

--- piggybank.sol
+++ piggybank.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 contract PiggyBank {
+    using SafeMath for uint256;
     
     address public checking_account;
     address public savings_account;
@@ -33,14 +36,14 @@ contract PiggyBank {
 
     function mint(uint amount, uint cap) public {
         assert(amount < cap);
-        balance[checking_account]+= amount;
+        balance[checking_account]= balance[checking_account].add(amount);
     }
 
     function sendToSavings(address from, address to, uint amount) public diffAcc(from, to) {
         from = checking_account;
         to = savings_account;
-        balance[from]-= amount;
-        balance[to]+= amount;
+        balance[from] = balance[from].sub(amount);
+        balance[to] = balance[to].add(amount);
         if(balance[from] < amount) {
             revert insufficientFunds();
         }
@@ -50,8 +53,8 @@ contract PiggyBank {
     function withdrawToChecking(address from, address to, uint amount) public diffAcc(from, to){
         from = savings_account;
         to = checking_account;
-        balance[from]-= amount;
-        balance[to]+= amount;
+        balance[from] = balance[from].sub(amount);
+        balance[to] = balance[to].add(amount);
         if(balance[from] < amount) {
             revert insufficientFunds();
         }
@@ -61,8 +64,8 @@ contract PiggyBank {
 
     function lockFunds(uint _amount, address from, address to, uint _locktime) public diffAcc(from, to) {
         to = locked;
-        balance[locked]+= _amount;
-        balance[from]-= _amount;
+        balance[locked] = balance[locked].add(_amount);
+        balance[from] = balance[from].sub(_amount);
         lockTime = block.timestamp + (_locktime  * 1 days);
     }
 
@@ -75,8 +78,7 @@ contract PiggyBank {
             emit fundsTransferred("You have successfully transferred your funds to oyur savings account");
         }
         //interest in gwei/wei
-       // uint interest = _interest *
-        //use safemath 
+       // uint interest = _interest * 
     }
 
     
