c464e688c519137a3d3a746ba73637d4e528fbe1
dear-ore <adorable2209@gmail.com>
2022-07-08 06:59:24 +0100

    Removed Safemath library and changed addresses to payable to receive ether

--- piggybank.sol
+++ piggybank.sol
@@ -1,86 +1,85 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract PiggyBank {
-    using SafeMath for uint256;
 
-    address public checking_account;
-    address public savings_account;
-    address public locked;
+
+    receive() external payable {}
+
+    address payable public checking_account;
+    address payable public savings_account;
+    address payable public locked;
+    address public owner = payable(msg.sender);
     uint public lockTime; //enter in days, get results in seconds/milliseconds
-    mapping(address => uint) balance;
     uint LT;
 
-    modifier diffAcc(address addr1, address addr2) {
+    modifier diffAcc(address payable addr1, address payable addr2) {
         require (addr1 != addr2);
         _;
     }
-    /*modifier onlyOwner() {
-        require(msg.sender == )
-    }*/
-   
-    constructor (address _checking, address _savings, address _locked) diffAcc(_checking, _savings) {
-        checking_account = _checking;
+
+    constructor (address payable _savings, address payable _locked) payable diffAcc(_savings, _locked) {
+        checking_account = payable(msg.sender);
         savings_account = _savings;
         locked = _locked;
     }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+   
+   /*modifier constructAdd (address payable _addr1, address payable _addr2, address payable _addr3) {
+       require(_addr1 != _addr2);
+       require(_addr1 != _addr3);
+       require(_addr2 != _addr3);
+       _;
+   }*/
 
     event fundsTransferred(string message);
     error insufficientFunds();
     error notYetTime();
 
-    function getBalance(address _user) public view returns(uint) {
-        return balance[_user];
+    function getBalance(address payable _user) public view returns(uint) {
+        return _user.balance;
     }
     
 
-    function mint(uint amount, uint cap) public {
-        assert(amount < cap);
-        balance[checking_account]= balance[checking_account].add(amount);
-    }
-
-    function sendToSavings(address from, address to, uint amount) public diffAcc(from, to) {
-        from = checking_account;
-        to = savings_account;
-        balance[from] = balance[from].sub(amount);
-        balance[to] = balance[to].add(amount);
-        if(balance[from] < amount) {
-            revert insufficientFunds();
-        }
+    function sendToSavings(address payable to, uint amount) payable public onlyOwner {
+        require(to == savings_account, "You have entered an incorrect address");
+        to.transfer(amount);
         emit fundsTransferred("Transfer successful");
     }
 
-    function withdrawToChecking(address from, address to, uint amount) public diffAcc(from, to){
-        from = savings_account;
-        to = checking_account;
-        balance[from] = balance[from].sub(amount);
-        balance[to] = balance[to].add(amount);
-        if(balance[from] < amount) {
+    function withdrawToChecking(address payable from, address payable to, uint amount) public onlyOwner diffAcc(from, to){
+        require(to == checking_account, "You have entered an incorrect address");
+        to.transfer(amount);
+        if (from.balance < amount) {
             revert insufficientFunds();
         }
         emit fundsTransferred("Transfer successful");
     }
 
 
-    function lockFunds(uint _amount, address from, address to, uint _locktime) public diffAcc(from, to) {
-        to = locked;
-        balance[locked] = balance[locked].add(_amount);
-        balance[from] = balance[from].sub(_amount);
+    function lockFunds(uint _amount, address payable from, address payable to, uint _locktime) payable public diffAcc(from, to) onlyOwner {
+        require(to == locked, "You have entered an incorrect address");
+        to.transfer(_amount);
         lockTime = _locktime  * 1 days;
         LT = block.timestamp + lockTime;
     }
 
-    function withdrawFromLocked (uint _amount) public {
+    function withdrawFromLocked (uint _amount) payable onlyOwner public {
         if(block.timestamp < LT){
             revert notYetTime();
         }
         else {
-            sendToSavings(locked, savings_account, _amount);
+            sendToSavings(savings_account, _amount);
             emit fundsTransferred("You have successfully transferred your funds to oyur savings account");
         }
-        //interest in gwei/wei
-       // uint interest = _interest * 
+    }
+
+    function getContractBalance () external view returns (uint){
+        return address(this).balance;
     }
 
     
